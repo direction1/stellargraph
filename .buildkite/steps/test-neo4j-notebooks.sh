5636608075dd3637e2455bc60c8ac051d7a9901e
Huon Wilson <Huon.Wilson@data61.csiro.au>
2020-03-10 12:02:10 +1100

    neo4j 3.5, absolute path

--- .buildkite/steps/test-neo4j-notebooks.sh
+++ .buildkite/steps/test-neo4j-notebooks.sh
@@ -9,7 +9,7 @@ pip install .
 echo "--- listing dependency versions"
 pip freeze
 
-directory="demos/connector/neo4j"
+directory="$PWD/demos/connector/neo4j"
 notebooks=('load-cora-into-neo4j.ipynb' 'directed-graphsage-on-cora-neo4j-example.ipynb' 'undirected-graphsage-on-cora-neo4j-example.ipynb')
 
 for name in "${notebooks[@]}"; do
